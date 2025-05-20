56c2eca9e8dbdc8a03d267a0f0c8b2b840796818
Dvl_Sant <jdsantana2003@gmail.com>
2025-05-20 09:10:15 -0700

    Primer commit: estructura base del proyecto con MarkerClustererF

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Mapa from './components/Mapa';
-import MapaConRuta from './components/Ruta'; // Importa el componente MapaConRuta
+import Home     from './components/Home';
+import About    from './components/About';
+import Contact  from './components/Contact';
+import Mapa     from './components/Mapa';
+import Ruta     from './components/Ruta';
+import MapaConClustering from './components/MarkerConClustering'; // CORRECTO
+
 import './index.css';
 
 function App() {
@@ -14,16 +16,18 @@ function App() {
         <Link to="/about">Acerca de</Link>
         <Link to="/contact">Contacto</Link>
         <Link to="/Mapa">Mapa</Link>
-        <Link to="/ruta">Ruta</Link> {/* Agrega un Link al componente Ruta */}
+        <Link to="/ruta">Ruta</Link> 
+        <Link to="/MapaClusters">Mapa con Clusters</Link> {/* Este enlace sigue bien */}
       </nav>
 
       <div className="main-container">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/Mapa" element={<Mapa />} />
-          <Route path="/ruta" element={<MapaConRuta />} /> {/* Agrega una Route para el componente MapaConRuta */}
+          <Route path="/"             element={<Home />} />
+          <Route path="/about"        element={<About />} />
+          <Route path="/contact"      element={<Contact />} />
+          <Route path="/Mapa"         element={<Mapa />} />
+          <Route path="/ruta"         element={<Ruta />} /> 
+          <Route path="/MapaClusters" element={<MapaConClustering />} /> {/* ACTUALIZADO */}
         </Routes>
       </div>
     </div>
